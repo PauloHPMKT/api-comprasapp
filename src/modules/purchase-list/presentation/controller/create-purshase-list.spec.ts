367ca5908efa028e23f59c19efb6f62489de5380
Paulo Sergio <pauloserg861@gmail.com>
2025-03-11 13:34:40 -0300

    test: ensure CreatePurshaseListController return 400 if no listName is privided

--- src/modules/purchase-list/presentation/controller/create-purshase-list.spec.ts
+++ src/modules/purchase-list/presentation/controller/create-purshase-list.spec.ts
@@ -9,4 +9,29 @@ describe('CreatePurchaseListController', () => {
     const sut = makeSut();
     expect(sut).toBeDefined();
   });
+
+  it('should return 400 if no list name is provided', async () => {
+    const sut = makeSut();
+    const httpRequest = {
+      body: {
+        items: [
+          {
+            product_id: 'any_id',
+            quantity: 3,
+            price: 10,
+            totalPricePerProduct: 30,
+          },
+          {
+            product_id: 'any_id',
+            quantity: 2,
+            price: 5,
+            totalPricePerProduct: 10,
+          },
+        ],
+      },
+    };
+    const response = await sut.handle(httpRequest);
+    expect(response.statusCode).toBe(400);
+    expect(response.body).toEqual(new Error('Missing param: listName'));
+  });
 });
