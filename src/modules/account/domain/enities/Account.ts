3e9ce58b856be635e51f6030d910ffde80b9abbe
Paulo Sergio <pauloserg861@gmail.com>
2025-03-16 16:28:48 -0300

    feat: ensure Account is created with a valid id

--- src/modules/account/domain/enities/Account.ts
+++ src/modules/account/domain/enities/Account.ts
@@ -1,1 +1,29 @@
-export class Account {}
+import { randomBytes } from 'crypto';
+
+export class Account {
+  public readonly id: string;
+  public userId: string;
+  public status?: Account.Status;
+  public plan?: Account.Plan;
+  public createdAt?: Date;
+  public updatedAt?: Date;
+
+  constructor(props: Account.Construct, id?: string) {
+    Object.assign(this, props);
+
+    this.id = id || this.generateId();
+    this.status = props.status ?? 'active';
+    this.plan = props.plan ?? 'free';
+    this.createdAt = props.createdAt ?? new Date();
+  }
+
+  private generateId(): string {
+    return randomBytes(12).toString('hex');
+  }
+}
+
+export namespace Account {
+  export type Status = 'active' | 'inactive';
+  export type Plan = 'free' | 'premium';
+  export type Construct = Omit<Account, 'id'>;
+}
