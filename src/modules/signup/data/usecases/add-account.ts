0050934a88f70f9dbb9e8154719a345913a953a0
Paulo Sergio <pauloserg861@gmail.com>
2025-03-16 14:34:04 -0300

    feat: ensure AddAccountUseCase calls Encrypter with correct password

--- src/modules/signup/data/usecases/add-account.ts
+++ src/modules/signup/data/usecases/add-account.ts
@@ -1,12 +1,19 @@
 import { AddAccount, AddAccountModel } from '../../domain/usecases/add-account';
 import { SignupDto } from '../dto/signup-dto';
+import { Encrypter } from '../protocols/encypter';
 
 export class AddAccountUseCase extends AddAccount {
+  constructor(private readonly encrypter: Encrypter) {
+    super();
+  }
+
   async add(accountData: SignupDto): Promise<AddAccountModel.Result | Error> {
     const passwordError = accountData.matchPassword();
     if (passwordError) {
       return passwordError;
     }
+
+    await this.encrypter.encrypt(accountData.password);
     return new Promise((resolve) =>
       resolve({
         id: 'valid_id',
