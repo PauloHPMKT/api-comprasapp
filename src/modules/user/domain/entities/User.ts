576538adb97454a4975379fad2a9456a9e4a0b4b
Paulo Sergio <pauloserg861@gmail.com>
2025-03-15 23:28:47 -0300

    feat: ensure User is created with a valid id

--- src/modules/user/domain/entities/User.ts
+++ src/modules/user/domain/entities/User.ts
@@ -1,1 +1,15 @@
-export class User {}
+import { randomBytes } from 'crypto';
+
+export class User {
+  public readonly id: string;
+  public name: string;
+  public email: string;
+  public password: string;
+  public createdAt?: Date;
+
+  constructor(props: Omit<User, 'id'>, id?: string) {
+    Object.assign(this, props);
+
+    this.id = id || randomBytes(12).toString('hex');
+  }
+}
