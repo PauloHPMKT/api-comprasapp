9efed2c9afedc1594fcaaa441e0dec32f855690b
Paulo Sergio <pauloserg861@gmail.com>
2025-03-16 14:52:18 -0300

    feat: ensure a user is created from User instance

--- src/modules/signup/data/usecases/add-account.ts
+++ src/modules/signup/data/usecases/add-account.ts
@@ -1,3 +1,4 @@
+import { User } from '@/modules/user/domain/entities/User';
 import { AddAccount, AddAccountModel } from '../../domain/usecases/add-account';
 import { SignupDto } from '../dto/signup-dto';
 import { Encrypter } from '../protocols/encypter';
@@ -13,7 +14,13 @@ export class AddAccountUseCase extends AddAccount {
       return passwordError;
     }
 
-    await this.encrypter.encrypt(accountData.password);
+    const hashedPassword = await this.encrypter.encrypt(accountData.password);
+    const user = new User({
+      name: accountData.name,
+      email: accountData.email,
+      password: hashedPassword,
+    });
+
     return new Promise((resolve) =>
       resolve({
         id: 'valid_id',
