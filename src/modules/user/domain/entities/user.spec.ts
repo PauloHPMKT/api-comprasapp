a0e8bb829deb8d59484c4a2f78bbbd9112043737
Paulo Sergio <pauloserg861@gmail.com>
2025-03-15 23:29:05 -0300

    test: ensure User is created with a valid id

--- src/modules/user/domain/entities/user.spec.ts
+++ src/modules/user/domain/entities/user.spec.ts
@@ -1,8 +1,22 @@
 import { User } from './User';
 
+const makeSut = (): User => {
+  return new User({
+    name: 'anyname',
+    email: 'anyemail@mail.com',
+    password: 'anypassword',
+  });
+};
+
 describe('User', () => {
   it('should be defined', () => {
-    const user = new User();
-    expect(user).toBeDefined();
+    const sut = makeSut();
+    expect(sut).toBeDefined();
+  });
+
+  it('should create a new user with an id', () => {
+    const sut = makeSut();
+    expect(sut.id).toBeDefined();
+    expect(sut.id).not.toBeNull();
   });
 });
