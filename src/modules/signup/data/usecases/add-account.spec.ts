32b701b29844cc089fb22481708f38b20511a33c
Paulo Sergio <pauloserg861@gmail.com>
2025-03-16 14:43:34 -0300

    test: ensure AddAccountUseCase throw if  Encrypter throws

--- src/modules/signup/data/usecases/add-account.spec.ts
+++ src/modules/signup/data/usecases/add-account.spec.ts
@@ -56,4 +56,21 @@ describe('AddAccount', () => {
     await sut.add(accountData);
     expect(encryptSpy).toHaveBeenCalledWith('hashed_password');
   });
+
+  it('should throw an erro if Encrypter throws', async () => {
+    const { sut, encrypterStub } = makeSut();
+    jest
+      .spyOn(encrypterStub, 'encrypt')
+      .mockReturnValueOnce(
+        new Promise((resolve, reject) => reject(new Error())),
+      );
+    const accountData = new SignupDto({
+      name: 'valid_name',
+      email: 'valid_email',
+      password: 'hashed_password',
+      passwordConfirmation: 'hashed_password',
+    });
+    const promise = sut.add(accountData);
+    await expect(promise).rejects.toThrow();
+  });
 });
