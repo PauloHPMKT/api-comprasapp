219adce6565400ac4c9f24de11bf46516327a563
Paulo Sergio <pauloserg861@gmail.com>
2025-03-16 16:29:24 -0300

    test: ensure Account is created with a valid id

--- src/modules/account/domain/enities/account.spec.ts
+++ src/modules/account/domain/enities/account.spec.ts
@@ -1,8 +1,20 @@
 import { Account } from './Account';
 
+const makeSut = (): Account => {
+  return new Account({
+    userId: 'valid_id',
+  });
+};
+
 describe('Account Entity', () => {
   it('should be defined', () => {
-    const sut = new Account();
+    const sut = makeSut();
     expect(sut).toBeDefined();
+  });
+
+  it('should return a valid account id', () => {
+    const sut = makeSut();
+    expect(sut.id).toBeTruthy();
+    expect(sut.id).not.toBeNull();
   });
 });
