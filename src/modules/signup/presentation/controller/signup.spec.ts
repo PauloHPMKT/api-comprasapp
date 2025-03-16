164a45e238a3cf9190fce8df6110be301e7cef54
Paulo Sergio <pauloserg861@gmail.com>
2025-03-15 21:07:30 -0300

    test: ensure SignupController return 400 if no name is provided

--- src/modules/signup/presentation/controller/signup.spec.ts
+++ src/modules/signup/presentation/controller/signup.spec.ts
@@ -1,8 +1,26 @@
 import { SignupController } from './signup';
 
+const makeSut = () => {
+  return new SignupController();
+};
+
 describe('SignupController', () => {
   it('should be defined', () => {
-    const sut = new SignupController();
+    const sut = makeSut();
     expect(sut).toBeDefined();
+  });
+
+  it('should return 400 if no name is provided', async () => {
+    const sut = makeSut();
+    const httpRequest = {
+      body: {
+        email: 'anyemail@mail.com',
+        password: 'anypassword',
+        passwordConfirmation: 'anypassword',
+      },
+    };
+    const httpResponse = await sut.handle(httpRequest);
+    expect(httpResponse.statusCode).toBe(400);
+    expect(httpResponse.body).toEqual(new Error('Missing params name'));
   });
 });
