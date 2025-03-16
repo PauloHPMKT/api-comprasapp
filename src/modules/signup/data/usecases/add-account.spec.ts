5eb37175873ee4304ea6225d8acbd147cec8db35
Paulo Sergio <pauloserg861@gmail.com>
2025-03-16 14:19:30 -0300

    test: ensure AddAccountUseCase return an error if password and passwordConfirmation not match

--- src/modules/signup/data/usecases/add-account.spec.ts
+++ src/modules/signup/data/usecases/add-account.spec.ts
@@ -1,4 +1,6 @@
+import { InvalidParamError } from '@/modules/shared/presentation/errors';
 import { AddAccountUseCase } from './add-account';
+import { SignupDto } from '../dto/signup-dto';
 
 const makeSut = (): AddAccountUseCase => {
   return new AddAccountUseCase();
@@ -8,5 +10,17 @@ describe('AddAccount', () => {
   it('should be defined', () => {
     const sut = makeSut();
     expect(sut).toBeDefined();
+  });
+
+  it('should return an error if password not match', async () => {
+    const sut = makeSut();
+    const accountData = new SignupDto({
+      name: 'anyname',
+      email: 'anyemail@mail.com',
+      password: 'anypassword',
+      passwordConfirmation: 'invalidpassword',
+    });
+    const response = await sut.add(accountData);
+    expect(response).toEqual(new InvalidParamError('passwordConfirmation'));
   });
 });
