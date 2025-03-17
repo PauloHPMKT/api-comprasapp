558f2b1fd2f6691175cb403a957bfd3ba17ea59a
Paulo Sergio <pauloserg861@gmail.com>
2025-03-17 13:50:56 -0300

    feat: ensure user is created with a valid Date

--- src/modules/user/domain/entities/User.ts
+++ src/modules/user/domain/entities/User.ts
@@ -3,10 +3,12 @@ interface UserProps {
   email: string;
   password: string;
   avatar?: string;
+  createdAt?: Date;
 }
 
 export class User {
   constructor(public readonly props: UserProps) {
     this.props.avatar = props.avatar ?? null;
+    this.props.createdAt = props.createdAt ?? new Date();
   }
 }
