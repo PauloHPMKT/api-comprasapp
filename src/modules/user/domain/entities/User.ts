cc58f6a217fcef39df873d07322aa18c3b3b9c99
Paulo Sergio <pauloserg861@gmail.com>
2025-03-17 13:45:36 -0300

    feat: ensure User is created with nullish avatar

--- src/modules/user/domain/entities/User.ts
+++ src/modules/user/domain/entities/User.ts
@@ -1,1 +1,12 @@
-export class User {}
+interface UserProps {
+  name: string;
+  email: string;
+  password: string;
+  avatar?: string;
+}
+
+export class User {
+  constructor(public readonly props: UserProps) {
+    this.props.avatar = props.avatar ?? null;
+  }
+}
