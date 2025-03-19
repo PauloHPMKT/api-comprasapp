81689acbe210ed8dbbbec1e7caa2a134cbb8b5b3
Paulo Sergio <pauloserg861@gmail.com>
2025-03-19 17:15:27 -0300

    test: ensure object value is setted

--- src/modules/user/domain/seedworks/domain/values-objects/value-object.spec.ts
+++ src/modules/user/domain/seedworks/domain/values-objects/value-object.spec.ts
@@ -6,8 +6,13 @@ const makeSut = (value: any) => {
 };
 
 describe('Value Object Base Class', () => {
-  it('should set value', () => {
+  it('should set string value', () => {
     const sut = makeSut('string value');
     expect(sut.value).toEqual('string value');
+  });
+
+  it('should set object value', () => {
+    const sut = makeSut({ key: 'value' });
+    expect(sut.value).toEqual({ key: 'value' });
   });
 });
