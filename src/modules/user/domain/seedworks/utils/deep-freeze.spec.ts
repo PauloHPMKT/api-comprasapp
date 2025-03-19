a5ad4ff80bd84a23331ca69472357c0adf0bd3d6
Paulo Sergio <pauloserg861@gmail.com>
2025-03-19 16:58:38 -0300

    test: ensure an object is not imutable

--- src/modules/user/domain/seedworks/utils/deep-freeze.spec.ts
+++ src/modules/user/domain/seedworks/utils/deep-freeze.spec.ts
@@ -20,4 +20,22 @@ describe('Object utils', () => {
     const bool = deepFreeze(false);
     expect(typeof bool).toBe('boolean');
   });
+
+  it('should be a imuatable object', () => {
+    const obj = deepFreeze({
+      prop1: 'value1',
+      deep: { prop2: 'value2', prop3: new Date() },
+    });
+    expect(() => {
+      (obj as any).prop1 = 'test';
+    }).toThrow(
+      "Cannot assign to read only property 'prop1' of object '#<Object>'",
+    );
+    expect(() => {
+      (obj as any).deep.prop2 = 'test';
+    }).toThrow(
+      "Cannot assign to read only property 'prop2' of object '#<Object>'",
+    );
+    expect(obj.deep.prop3).toBeInstanceOf(Date);
+  });
 });
