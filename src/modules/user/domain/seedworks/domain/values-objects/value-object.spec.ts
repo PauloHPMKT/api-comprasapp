cf848fcc7ed520c6e842b1659cd4528b38a85046
Paulo Sergio <pauloserg861@gmail.com>
2025-03-19 17:19:06 -0300

    test: ensure ValueObject convert values

--- src/modules/user/domain/seedworks/domain/values-objects/value-object.spec.ts
+++ src/modules/user/domain/seedworks/domain/values-objects/value-object.spec.ts
@@ -15,4 +15,26 @@ describe('Value Object Base Class', () => {
     const sut = makeSut({ key: 'value' });
     expect(sut.value).toEqual({ key: 'value' });
   });
+
+  it('should convert object value to string', () => {
+    const date = new Date();
+    // eslint-disable-next-line prefer-const
+    let arrange = [
+      { received: 'value', expected: 'value' },
+      { received: 1.1, expected: '1.1' },
+      { received: true, expected: 'true' },
+      { received: false, expected: 'false' },
+      {
+        received: { prop1: 'value1' },
+        expected: JSON.stringify({ prop1: 'value1' }),
+      },
+      { received: date, expected: date.toString() },
+      { received: { key: 'value' }, expected: '{"key":"value"}' },
+    ];
+
+    arrange.forEach((value) => {
+      const vo = makeSut(value.received);
+      expect(vo + '').toBe(value.expected);
+    });
+  });
 });
