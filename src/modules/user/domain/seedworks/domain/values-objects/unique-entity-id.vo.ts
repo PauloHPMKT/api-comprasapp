d8fb22415e762f59897f1dad22a10762deccbaf4
Paulo Sergio <pauloserg861@gmail.com>
2025-03-19 15:51:28 -0300

    feat: ensure UniqueEntityId thorw an erro if a invalid id is provided

--- src/modules/user/domain/seedworks/domain/values-objects/unique-entity-id.vo.ts
+++ src/modules/user/domain/seedworks/domain/values-objects/unique-entity-id.vo.ts
@@ -1,6 +1,16 @@
+import { randomBytes } from 'crypto';
+
 export class UniqueEntityId {
   private readonly id: string;
   constructor(id: string) {
-    this.id = id;
+    this.id = id || randomBytes(12).toString('hex');
+    this.validate();
+  }
+
+  private validate() {
+    const idRegex = /^[0-9a-fA-F]{24}$/;
+    if (!idRegex.test(this.id)) {
+      throw new Error('Invalid id');
+    }
   }
 }
