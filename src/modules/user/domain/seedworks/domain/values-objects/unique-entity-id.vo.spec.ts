3fc7b37d27c9f5acf24f52c216e8b61e84804e7a
Paulo Sergio <pauloserg861@gmail.com>
2025-03-19 15:58:46 -0300

    test: ensure UniqueEntityId calls validate method if an invalid id is provided

--- src/modules/user/domain/seedworks/domain/values-objects/unique-entity-id.vo.spec.ts
+++ src/modules/user/domain/seedworks/domain/values-objects/unique-entity-id.vo.spec.ts
@@ -1,3 +1,4 @@
+import { UniqueEntityIdError } from '../../errors/unique-entity-id-error';
 import { UniqueEntityId } from './unique-entity-id.vo';
 
 const makeSut = (id?: string): UniqueEntityId => {
@@ -13,6 +14,13 @@ describe('UniqueEntityId Value Object', () => {
   });
 
   it('should throw error if id is invalid', () => {
-    expect(() => makeSut('invalid_id')).toThrow('ID must be valid');
+    expect(() => makeSut('invalid_id')).toThrow(new UniqueEntityIdError());
+  });
+
+  it('should check if validate method is called', () => {
+    const validateSpy = jest.spyOn(UniqueEntityId.prototype as any, 'validate');
+    expect(() => makeSut('invalid_id')).toThrow(new UniqueEntityIdError());
+    expect(validateSpy).toHaveBeenCalled();
+    expect(validateSpy).toHaveBeenCalledTimes(1);
   });
 });
