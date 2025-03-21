b7dcdc58ae45a1eb8779bde5d9ceb42e529ebf56
Paulo Sergio <pauloserg861@gmail.com>
2025-03-21 08:05:13 -0300

    feat: ensure a new Account is created with a free plan initialy

--- src/modules/account/domain/entities/Acount.ts
+++ src/modules/account/domain/entities/Acount.ts
@@ -1,10 +1,16 @@
 export type AccountProps = {
   userId: string;
   isActive?: boolean;
+  plan: Account.Plan;
 };
+
+export namespace Account {
+  export type Plan = 'free' | 'premium';
+}
 
 export class Account {
   constructor(public readonly props: AccountProps) {
-    this.props.isActive = props.isActive || true;
+    this.props.isActive = props.isActive ?? true;
+    this.props.plan = props.plan ?? 'free';
   }
 }
