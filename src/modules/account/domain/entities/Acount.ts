4fbcfdeb55e209eaaa61c4087623061df878b060
Paulo Sergio <pauloserg861@gmail.com>
2025-03-21 07:53:44 -0300

    feat: ensure a new Account is created active as default

--- src/modules/account/domain/entities/Acount.ts
+++ src/modules/account/domain/entities/Acount.ts
@@ -1,7 +1,10 @@
 export type AccountProps = {
   userId: string;
+  isActive?: boolean;
 };
 
 export class Account {
-  constructor(public readonly props: AccountProps) {}
+  constructor(public readonly props: AccountProps) {
+    this.props.isActive = props.isActive || true;
+  }
 }
