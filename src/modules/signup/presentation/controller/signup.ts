bdc445197e2e2f994d1d6763ddf92135ec7efda0
Paulo Sergio <pauloserg861@gmail.com>
2025-03-22 13:05:21 -0300

    feat: ensure SignupController return 400 if no name is provided

--- src/modules/signup/presentation/controller/signup.ts
+++ src/modules/signup/presentation/controller/signup.ts
@@ -1,1 +1,15 @@
-export class SignupController {}
+interface HttpResponse {
+  statusCode: number;
+  body: any;
+}
+
+export class SignupController {
+  async handle(httpRequest: any): Promise<HttpResponse> {
+    if (!httpRequest.body.name) {
+      return {
+        statusCode: 400,
+        body: new Error('Missing param name'),
+      };
+    }
+  }
+}
