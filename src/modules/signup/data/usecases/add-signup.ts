8c39607501a8531a15c7949fd5fb4608d262a196
Paulo Sergio <pauloserg861@gmail.com>
2025-03-22 20:54:44 -0300

    feat: ensure AddSignupUseCase throws if passwords not match

--- src/modules/signup/data/usecases/add-signup.ts
+++ src/modules/signup/data/usecases/add-signup.ts
@@ -1,1 +1,35 @@
-export class AddSignupUseCase {}
+import { AddSignup } from '../../domain/usecases/add-signup';
+import { SignupModel } from '../models/add-signup';
+
+export class AddSignupUseCase implements AddSignup {
+  async add(params: SignupModel.Params): Promise<SignupModel.Result> {
+    if (params.password !== params.passwordConfirmation) {
+      throw new Error('Invalid Param: passwordConfirmation');
+    }
+
+    // comparar as senhas
+
+    // verificar se o email já existe
+
+    // criptografar a senha
+
+    // criar um novo usuário
+
+    // criar uma conta para o usuário e vincular ao usuário
+
+    // salvar no banco de dados
+
+    // retornar o usuário
+    return new Promise((resolve) =>
+      resolve({
+        id: 'any_id',
+        name: 'any_name',
+        email: 'any_email@mail.com',
+        password: 'any_password',
+        avatar: 'null',
+        accountId: 'any_account_id',
+        createdAt: new Date('2025-12-10'),
+      }),
+    );
+  }
+}
