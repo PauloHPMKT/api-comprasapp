40a037b5543d426ac428a21c96684f585a3bae50
Paulo Sergio <pauloserg861@gmail.com>
2025-03-22 11:55:45 -0300

    feat: ensure Account is created with a valid id

--- src/modules/account/domain/entities/Acount.ts
+++ src/modules/account/domain/entities/Acount.ts
@@ -1,3 +1,6 @@
+import Entity from '@/shared/seedworks/domain/entity/entity';
+import { UniqueEntityId } from '@/shared/seedworks/domain/values-objects/unique-entity-id.vo';
+
 export type AccountProps = {
   userId: string;
   isActive?: boolean;
@@ -9,8 +12,12 @@ export namespace Account {
   export type Plan = 'free' | 'premium';
 }
 
-export class Account {
-  constructor(public readonly props: AccountProps) {
+export class Account extends Entity<AccountProps> {
+  constructor(
+    public readonly props: AccountProps,
+    id?: UniqueEntityId,
+  ) {
+    super(props, id);
     this.props.isActive = props.isActive ?? true;
     this.props.plan = props.plan ?? 'free';
     this.props.createdAt = props.createdAt ?? new Date();
