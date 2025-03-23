50dc1d50f80e55ec47e61bd48c38ac02cd9362a5
Paulo Sergio <pauloserg861@gmail.com>
2025-03-22 21:07:07 -0300

    test: ensure AddSignupUseCase calls Encrypter with correct value

--- src/modules/signup/data/usecases/add-signup.spec.ts
+++ src/modules/signup/data/usecases/add-signup.spec.ts
@@ -1,19 +1,39 @@
+import { Encrypter } from '../protocols/encrypter';
 import { AddSignupUseCase } from './add-signup';
 
-const makeSut = (): AddSignupUseCase => {
-  return new AddSignupUseCase();
+const makeEncrypter = (): Encrypter => {
+  class EncrypterStub implements Encrypter {
+    async encrypt(value: string): Promise<string> {
+      return new Promise((resolve) => resolve('hashed_password'));
+    }
+  }
+  return new EncrypterStub();
+};
+
+const makeSut = (): SutTypes => {
+  const encrypterStub = makeEncrypter();
+  const sut = new AddSignupUseCase(encrypterStub);
+  return {
+    sut,
+    encrypterStub,
+  };
+};
+
+type SutTypes = {
+  sut: AddSignupUseCase;
+  encrypterStub: Encrypter;
 };
 
 describe('AddSignupUseCase', () => {
   it('should be defined', () => {
-    const sut = makeSut();
+    const { sut } = makeSut();
     expect(sut).toBeDefined();
     expect(sut).toBeInstanceOf(AddSignupUseCase);
     expect(sut).toBeTruthy();
   });
 
   it('should throw if the password and passwordConfirmation not match', async () => {
-    const sut = makeSut();
+    const { sut } = makeSut();
     const params = {
       name: 'anyname',
       email: 'anyemail@mail.com',
@@ -24,5 +44,18 @@ describe('AddSignupUseCase', () => {
     await expect(promise).rejects.toThrow(
       'Invalid Param: passwordConfirmation',
     );
+  });
+
+  it('should call Encrypter with correct password', async () => {
+    const { sut, encrypterStub } = makeSut();
+    const encryptSpy = jest.spyOn(encrypterStub, 'encrypt');
+    const params = {
+      name: 'anyname',
+      email: 'anyemail@mail.com',
+      password: 'anypassword',
+      passwordConfirmation: 'anypassword',
+    };
+    await sut.add(params);
+    expect(encryptSpy).toHaveBeenCalledWith('anypassword');
   });
 });
