ab5c73457e5a176701451f1dd77f504d7b7b19ab
Paulo Sergio <pauloserg861@gmail.com>
2025-03-26 08:29:39 -0300

    feat: ensure ContentType set json as default and force another format

--- src/main/configs/middlewares.ts
+++ src/main/configs/middlewares.ts
@@ -1,8 +1,9 @@
 import { Application } from 'express';
 import { cors } from '../middlewares/cors';
 import { bodyParser } from '../middlewares/body-parser';
+import { contentType } from '../middlewares/content-type';
 
 export default (app: Application): void => {
-  const middlewares = [cors, bodyParser];
+  const middlewares = [cors, bodyParser, contentType];
   middlewares.forEach((middleware) => app.use(middleware));
 };
