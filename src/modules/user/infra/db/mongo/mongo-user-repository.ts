2684fa9b04ef30aee8579f13a718f04a22c56e44
Paulo Sergio <pauloserg861@gmail.com>
2025-03-25 23:04:35 -0300

    feat: ensure MongoUserRepository return true if an user exist

--- src/modules/user/infra/db/mongo/mongo-user-repository.ts
+++ src/modules/user/infra/db/mongo/mongo-user-repository.ts
@@ -1,1 +1,10 @@
-export class MongoUserRepository {}
+import { VerifyEmailRepository } from '../../../data/protocols';
+import { MongoHelper } from '../helper/mongo-client';
+
+export class MongoUserRepository implements VerifyEmailRepository {
+  async verify(email: string): Promise<boolean> {
+    const getCollection = MongoHelper.getCollection('users');
+    const isUser = await getCollection.findOne({ email });
+    return !!isUser;
+  }
+}
