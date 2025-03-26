11bdc9af933cff2d5e839b78848a00a8ce342bc3
Paulo Sergio <pauloserg861@gmail.com>
2025-03-26 07:03:34 -0300

    feat: ensure MongoUserRepository return a user on success

--- src/modules/user/infra/db/mongo/mongo-user-repository.ts
+++ src/modules/user/infra/db/mongo/mongo-user-repository.ts
@@ -1,10 +1,32 @@
-import { VerifyEmailRepository } from '../../../data/protocols';
+import { UserModel } from '../../../domain/models/user-model';
+import {
+  VerifyEmailRepository,
+  AddUserRepository,
+} from '../../../data/protocols';
 import { MongoHelper } from '../helper/mongo-client';
 
-export class MongoUserRepository implements VerifyEmailRepository {
+export class MongoUserRepository
+  implements VerifyEmailRepository, AddUserRepository
+{
   async verify(email: string): Promise<boolean> {
     const getCollection = MongoHelper.getCollection('users');
     const isUser = await getCollection.findOne({ email });
     return !!isUser;
+  }
+
+  async create(data: UserModel.Params): Promise<UserModel.Params> {
+    const userCollection = MongoHelper.getCollection('users');
+    const { insertedId } = await userCollection.insertOne({
+      _id: data.id,
+      name: data.name,
+      email: data.email,
+      password: data.password,
+      avatar: data.avatar,
+      accountId: data.accountId,
+      createdAt: data.createdAt,
+    });
+
+    const user = await userCollection.findOne({ _id: insertedId });
+    return MongoHelper.map(user);
   }
 }
