127f00b883a499a9dd8f670db78487ec8b2ad388
Paulo Sergio <pauloserg861@gmail.com>
2025-03-26 08:25:27 -0300

    feat: add mongodb connection to init server

--- src/main/server.ts
+++ src/main/server.ts
@@ -1,5 +1,11 @@
+import { MongoHelper } from '@/shared/infra/db/helper/mongo-client';
 import { App } from './framework/app';
 
-const port = 3005;
-const app = new App();
-app.initServer(port);
+MongoHelper.connect('mongodb://localhost:27017/comprasapp')
+  .then(() => {
+    const port = 3005;
+    const app = new App();
+    app.initServer(port);
+    console.log('Connected to MongoDB');
+  })
+  .catch(console.error);
