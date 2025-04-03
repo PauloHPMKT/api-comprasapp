f5575f11a41b050ef218aad9b23f038dd312b62b
Paulo Sergio <pauloserg861@gmail.com>
2025-04-03 16:40:44 -0300

    test: ensure a purchase list is created with a list of products

--- src/modules/purchase-list/domain/entities/purchase-list.spec.ts
+++ src/modules/purchase-list/domain/entities/purchase-list.spec.ts
@@ -3,6 +3,18 @@ import { ProductProps, PurchaseList } from './PurchaseList';
 const makeSut = (): PurchaseList => {
   const purchaseList: ProductProps = {
     title: 'List title',
+    products: [
+      {
+        name: 'Product 1',
+        quantity: 2,
+        price: 10,
+      },
+      {
+        name: 'Product 2',
+        quantity: 1,
+        price: 20,
+      },
+    ],
   };
   const sut = new PurchaseList(purchaseList);
   return sut;
@@ -20,5 +32,24 @@ describe('PurchaseList', () => {
     const sut = makeSut();
     const listName = 'List title';
     expect(sut.props.title).toEqual(listName);
+  });
+
+  it('should create a purchase list with a list of products', () => {
+    const sut = makeSut();
+    const products = [
+      {
+        name: 'Product 1',
+        quantity: 2,
+        price: 10,
+      },
+      {
+        name: 'Product 2',
+        quantity: 1,
+        price: 20,
+      },
+    ];
+    expect(sut.props.products).not.toBeUndefined();
+    expect(sut.props.products).not.toBeNull();
+    expect(sut.props.products).toEqual(products);
   });
 });
