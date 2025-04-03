d988595f388ccc428b8f0d9648c4b1b7b08871fb
Paulo Sergio <pauloserg861@gmail.com>
2025-04-03 16:40:02 -0300

    feat: ensure a purchase list is created with a list of products

--- src/modules/purchase-list/domain/entities/PurchaseList.ts
+++ src/modules/purchase-list/domain/entities/PurchaseList.ts
@@ -1,7 +1,16 @@
 export type ProductProps = {
   title: string;
+  products: Products.toCreate[];
 };
 
 export class PurchaseList {
   constructor(public readonly props: ProductProps) {}
+}
+
+export namespace Products {
+  export type toCreate = {
+    name: string;
+    quantity: number;
+    price: number;
+  };
 }
