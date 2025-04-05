439a923054d8ee02e3c8d37331592987e402507f
Paulo Sergio <pauloserg861@gmail.com>
2025-04-05 17:12:41 -0300

    refactor: implement models to transport Purchase List data

---
+++ src/modules/purchase-list/domain/models/create-purchase-list.ts
@@ -0,0 +1,18 @@
+import { Products } from '../entities/PurchaseList';
+
+export namespace PurchaseListModel {
+  export interface Params {
+    title: string;
+    description?: string | null;
+    products: Products.toCreate[];
+  }
+
+  export type Result = {
+    id: string;
+    title: string;
+    description: string | null;
+    products: Products.toCreate[];
+    createdAt: Date;
+    updatedAt: Date | null;
+  };
+}
--- src/modules/purchase-list/presentation/controller/create-purchase-list.ts
+++ src/modules/purchase-list/presentation/controller/create-purchase-list.ts
@@ -1,26 +1,16 @@
 import { MissingParamError } from '@/shared/presentation/errors';
 import { Controller } from '@/shared/presentation/protocols/controller';
 import { HttpRequest, HttpResponse } from '@/shared/presentation/types/http';
-
-interface CreateListDto {
-  title: string;
-  description: string | null;
-  products: Products[];
-}
-
-interface Products {
-  name: string;
-  quantity: number;
-  unitPrice?: number | null;
-  totalPrice?: number | null;
-}
+import { PurchaseListModel } from '../../domain/models/create-purchase-list';
 
 export class CreatePurchaseListController extends Controller {
   constructor() {
     super();
   }
 
-  async handle(httpRequest: HttpRequest<CreateListDto>): Promise<HttpResponse> {
+  async handle(
+    httpRequest: HttpRequest<PurchaseListModel.Params>,
+  ): Promise<HttpResponse> {
     const { title, description, products } = httpRequest.body;
     const requiredFields = ['title', 'products'];
     const error = this.validateRequiredFields(httpRequest, requiredFields);
