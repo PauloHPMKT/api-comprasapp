89a75165c8715f807a35edfed0ddb3597c22bfac
Paulo Sergio <pauloserg861@gmail.com>
2025-04-06 15:06:55 -0300

    feat: ensure MongoPurchaseListRepository return true when verify method found a list with same name

--- src/modules/purchase-list/infra/db/purchase-list/mongo-purchase-list.ts
+++ src/modules/purchase-list/infra/db/purchase-list/mongo-purchase-list.ts
@@ -1,1 +1,11 @@
-export class MongoPurchaseListRepository {}
+import { MongoHelper } from '@/shared/infra/db/helper/mongo-client';
+import { VerifyListRepository } from '../../../data/protocols/verify-list-repository';
+
+export class MongoPurchaseListRepository implements VerifyListRepository {
+  async verify(title: string): Promise<boolean> {
+    const purchaseList = await MongoHelper.getCollection(
+      'purchase-list',
+    ).findOne({ title });
+    return !!purchaseList;
+  }
+}
