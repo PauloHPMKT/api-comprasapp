59e960bca48444bdf81fcccb696cba722a3ffec0
Paulo Sergio <pauloserg861@gmail.com>
2025-04-06 11:53:35 -0300

    test: ensure AddPurchaseListUseCase instantiate correctly PurchaseList entity

--- src/modules/purchase-list/data/usecases/add-purchase-list.spec.ts
+++ src/modules/purchase-list/data/usecases/add-purchase-list.spec.ts
@@ -1,6 +1,35 @@
+import { PurchaseList } from '../../domain/entities/PurchaseList';
 import { AddPurchaseListRepository } from '../protocols/add-purchase-list-repository';
 import { VerifyListRepository } from '../protocols/verify-list-repository';
 import { AddPurchaseListUseCase } from './add-purchase-list';
+
+const mockPurchaseList = {
+  title: 'valid_list_title',
+  description: null,
+  products: [
+    {
+      name: 'Product 1',
+      quantity: 2,
+      unitPrice: null,
+      totalPrice: null,
+    },
+    {
+      name: 'Product 2',
+      quantity: 1,
+      unitPrice: 20,
+      totalPrice: 20,
+    },
+  ],
+  userId: 'valid_user_id',
+};
+
+jest.mock('../../domain/entities/PurchaseList', () => {
+  return {
+    PurchaseList: jest.fn().mockImplementation(() => {
+      return mockPurchaseList;
+    }),
+  };
+});
 
 const makeVerifyListStub = (): VerifyListRepository => {
   class VerifyListStub implements VerifyListRepository {
@@ -14,28 +43,7 @@ const makeVerifyListStub = (): VerifyListRepository => {
 const makeAddPurchaseListRepository = (): AddPurchaseListRepository => {
   class AddPurchaseListRepositoryStub implements AddPurchaseListRepository {
     async addList(data: any): Promise<any> {
-      return Promise.resolve({
-        id: 'valid_id',
-        title: 'valid_list_title',
-        description: null,
-        products: [
-          {
-            name: 'Product 1',
-            quantity: 2,
-            unitPrice: null,
-            totalPrice: null,
-          },
-          {
-            name: 'Product 2',
-            quantity: 1,
-            unitPrice: 20,
-            totalPrice: 20,
-          },
-        ],
-        userId: 'valid_user_id',
-        createdAt: new Date('2025-10-01'),
-        updatedAt: null,
-      });
+      return Promise.resolve(mockPurchaseList);
     }
   }
   return new AddPurchaseListRepositoryStub();
@@ -111,5 +119,36 @@ describe('AddPurchaseListUseCase', () => {
     await expect(response).rejects.toThrow(
       new Error('A purchase list with this title already exists'),
     );
+  });
+
+  it('should instantiate PurchaseList entity with correct values', async () => {
+    const { sut } = makeSut();
+    const params = {
+      title: 'anytitle',
+      description: 'anydescription',
+      products: [
+        {
+          name: 'Product 1',
+          quantity: 2,
+          unitPrice: 10,
+          totalPrice: 20,
+        },
+      ],
+      userId: 'anyuserid',
+    };
+    await sut.add(params);
+    expect(PurchaseList).toHaveBeenCalledWith({
+      title: 'anytitle',
+      description: 'anydescription',
+      products: [
+        {
+          name: 'Product 1',
+          quantity: 2,
+          unitPrice: 10,
+          totalPrice: 20,
+        },
+      ],
+      userId: 'anyuserid',
+    });
   });
 });
