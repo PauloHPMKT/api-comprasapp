65ac064bda5c13badf855c54e59eb426a52632c1
Paulo Sergio <pauloserg861@gmail.com>
2025-04-06 11:55:29 -0300

    feat: ensure a purchaselist is returned on success

--- src/modules/purchase-list/data/usecases/add-purchase-list.ts
+++ src/modules/purchase-list/data/usecases/add-purchase-list.ts
@@ -26,29 +26,6 @@ export class AddPurchaseListUseCase implements AddPurchaseList {
 
     const createList =
       await this.addPurchaseListRepository.addList(purchaseList);
-    return new Promise((resolve) =>
-      resolve({
-        id: 'valid_id',
-        title: 'valid_list_title',
-        description: null,
-        products: [
-          {
-            name: 'Product 1',
-            quantity: 2,
-            unitPrice: null,
-            totalPrice: null,
-          },
-          {
-            name: 'Product 2',
-            quantity: 1,
-            unitPrice: 20,
-            totalPrice: 20,
-          },
-        ],
-        userId: 'valid_user_id',
-        createdAt: new Date('2025-10-01'),
-        updatedAt: null,
-      }),
-    );
+    return createList;
   }
 }
