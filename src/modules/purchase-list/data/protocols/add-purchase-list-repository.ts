be48718ce7104103e5e63133a2581bb87a2986af
Paulo Sergio <pauloserg861@gmail.com>
2025-04-06 10:46:13 -0300

    feat: ensure AddPurchaseListUseCase calls AddPurchaseListRepository with correct values

---
+++ src/modules/purchase-list/data/protocols/add-purchase-list-repository.ts
@@ -0,0 +1,3 @@
+export interface AddPurchaseListRepository {
+  addList(data: any): Promise<any>;
+}
--- src/modules/purchase-list/data/usecases/add-purchase-list.ts
+++ src/modules/purchase-list/data/usecases/add-purchase-list.ts
@@ -1,1 +1,36 @@
-export class AddPurchaseListUseCase {}
+import { AddPurchaseList } from '../../domain/usecases/add-purchase-list';
+import { AddPurchaseListRepository } from '../protocols/add-purchase-list-repository';
+
+export class AddPurchaseListUseCase implements AddPurchaseList {
+  constructor(
+    private readonly addPurchaseListRepository: AddPurchaseListRepository,
+  ) {}
+
+  async add(data: any): Promise<any> {
+    await this.addPurchaseListRepository.addList(data);
+    return new Promise((resolve) =>
+      resolve({
+        id: 'valid_id',
+        title: 'valid_list_title',
+        description: null,
+        products: [
+          {
+            name: 'Product 1',
+            quantity: 2,
+            unitPrice: null,
+            totalPrice: null,
+          },
+          {
+            name: 'Product 2',
+            quantity: 1,
+            unitPrice: 20,
+            totalPrice: 20,
+          },
+        ],
+        userId: 'valid_user_id',
+        createdAt: new Date('2025-10-01'),
+        updatedAt: null,
+      }),
+    );
+  }
+}
