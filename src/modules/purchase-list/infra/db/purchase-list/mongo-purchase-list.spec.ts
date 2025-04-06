00c672cc980b92b5aa682affc74276fe4ff945e0
Paulo Sergio <pauloserg861@gmail.com>
2025-04-06 15:07:19 -0300

    test: ensure MongoPurchaseListRepository return true when verify method found a list with same name

--- src/modules/purchase-list/infra/db/purchase-list/mongo-purchase-list.spec.ts
+++ src/modules/purchase-list/infra/db/purchase-list/mongo-purchase-list.spec.ts
@@ -1,3 +1,4 @@
+import { MongoHelper } from '@/shared/infra/db/helper/mongo-client';
 import { MongoPurchaseListRepository } from './mongo-purchase-list';
 
 const makeSut = (): MongoPurchaseListRepository => {
@@ -5,10 +6,44 @@ const makeSut = (): MongoPurchaseListRepository => {
 };
 
 describe('MongoPurchaseListRepository', () => {
+  beforeAll(async () => {
+    await MongoHelper.connect(process.env.MONGO_URL as string);
+  });
+
+  afterAll(async () => {
+    await MongoHelper.disconnect();
+  });
+
+  beforeEach(async () => {
+    const accountCollection = MongoHelper.getCollection('users');
+    await accountCollection.deleteMany({});
+  });
+
   it('should be defined', () => {
     const sut = makeSut();
     expect(sut).toBeDefined();
     expect(sut).toBeInstanceOf(MongoPurchaseListRepository);
     expect(sut).toBeTruthy();
+  });
+
+  it('should return true if a purchase list exists with the same title', async () => {
+    const sut = makeSut();
+    const getCollection = MongoHelper.getCollection('purchase-list');
+    await getCollection.insertOne({
+      id: 'valid_id',
+      title: 'anytitle',
+      description: 'anydescription',
+      products: [
+        {
+          name: 'Product 1',
+          quantity: 2,
+          unitPrice: 10,
+          totalPrice: 20,
+        },
+      ],
+      userId: 'anyuserid',
+    });
+    const isList = await sut.verify('anytitle');
+    expect(isList).toBe(true);
   });
 });
