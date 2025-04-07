ebb83c598b5fea367f9b74b7ce6503397193fe9c
Paulo Sergio <pauloserg861@gmail.com>
2025-04-07 11:46:50 -0300

    feat: ensure AuthController return 400 if no email is provided

--- src/modules/auth/presentation/controllers/auth.ts
+++ src/modules/auth/presentation/controllers/auth.ts
@@ -1,1 +1,12 @@
-export class AuthController {}
+import { MissingParamError } from '@/shared/presentation/errors';
+import { badRequest } from '@/shared/presentation/helper/http-responses';
+import { Controller } from '@/shared/presentation/protocols/controller';
+import { HttpResponse } from '@/shared/presentation/types/http';
+
+export class AuthController extends Controller {
+  async handle(httpRequest: any): Promise<HttpResponse> {
+    if (!httpRequest.body.email) {
+      return badRequest(new MissingParamError('email'));
+    }
+  }
+}
