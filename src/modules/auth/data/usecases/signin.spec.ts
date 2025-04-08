f2cc499078da5003699ac4a3f12922846bd252ab
Paulo Sergio <pauloserg861@gmail.com>
2025-04-08 15:59:14 -0300

    test: ensure SignInUseCase return a valid aceess token and user structure from payload

--- src/modules/auth/data/usecases/signin.spec.ts
+++ src/modules/auth/data/usecases/signin.spec.ts
@@ -1,14 +1,27 @@
+import { GenerateToken } from '../protocols/generate-token';
 import { SignInUseCase } from './signin';
 
+const makeGenerateWebtoken = (): GenerateToken => {
+  class GenerateWebtoken implements GenerateToken {
+    async sign(payload: any): Promise<string> {
+      return new Promise((resolve) => resolve('valid_token'));
+    }
+  }
+  return new GenerateWebtoken();
+};
+
 const makeSut = (): SutTypes => {
-  const sut = new SignInUseCase();
+  const generateWebtokenStub = makeGenerateWebtoken();
+  const sut = new SignInUseCase(generateWebtokenStub);
   return {
     sut,
+    generateWebtokenStub,
   };
 };
 
 type SutTypes = {
   sut: SignInUseCase;
+  generateWebtokenStub: GenerateToken;
 };
 
 describe('SginInUseCase', () => {
@@ -17,5 +30,30 @@ describe('SginInUseCase', () => {
     expect(sut).toBeDefined();
     expect(sut).toBeInstanceOf(SignInUseCase);
     expect(sut).toBeTruthy();
+  });
+
+  it('should return access token from payload', async () => {
+    const { sut, generateWebtokenStub } = makeSut();
+    jest.spyOn(generateWebtokenStub, 'sign');
+    const params = {
+      id: 'valid_id',
+      name: 'valid_name',
+      email: 'valid_email@mail.com',
+      avatar: null,
+      accountId: 'valid_account_id',
+      createdAt: new Date('2025-01-01'),
+    };
+    const result = await sut.login(params);
+    expect(result).toEqual({
+      user: {
+        id: 'valid_id',
+        name: 'valid_name',
+        email: 'valid_email@mail.com',
+        avatar: null,
+        accountId: 'valid_account_id',
+        createdAt: new Date('2025-01-01'),
+      },
+      access_token: 'valid_token',
+    });
   });
 });
