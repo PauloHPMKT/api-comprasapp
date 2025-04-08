b8b17054c39d8639bd60a2d82c1de55e2e84c934
Paulo Sergio <pauloserg861@gmail.com>
2025-04-08 15:52:11 -0300

    feat: ensure SignInUseCase return  webtoken on success

--- src/modules/auth/data/usecases/signin.ts
+++ src/modules/auth/data/usecases/signin.ts
@@ -1,1 +1,29 @@
-export class SignInUseCase {}
+import { AuthSignInModel } from '../../domain/models/auth-signin';
+import { SignIn } from '../../domain/usecases/signin';
+import { GenerateToken } from '../protocols/generate-token';
+
+export class SignInUseCase implements SignIn {
+  constructor(private readonly generateWebtoken: GenerateToken) {}
+
+  async login(
+    data: AuthSignInModel.SignIn,
+  ): Promise<AuthSignInModel.SignInResult> {
+    const payload = {
+      id: data.id,
+      name: data.name,
+      email: data.email,
+      avatar: data.avatar,
+      accountId: data.accountId,
+      createdAt: data.createdAt,
+    };
+
+    await this.generateWebtoken.sign(payload);
+
+    return new Promise((resolve) =>
+      resolve({
+        user: payload,
+        access_token: 'valid_token',
+      }),
+    );
+  }
+}
