55e9fae4bd11808e74f3113aa41a7c4795ebd9de
Paulo Sergio <pauloserg861@gmail.com>
2025-04-07 22:18:42 -0300

    feat: ensure AuthSigninUseCase return null if any credentials are invalid

--- src/modules/auth/data/usecases/auth-signin.ts
+++ src/modules/auth/data/usecases/auth-signin.ts
@@ -1,28 +1,25 @@
 import { AuthSignInModel } from '../../domain/models/auth-signin';
 import { SignIn } from '../../domain/usecases/auth-signin';
 import { FindUserByEmailRepository } from '@/shared/services/user/protocols/find-user-by-email';
+import { CompareCrypto } from '@/modules/signup/data/protocols/compare-crypto';
 
 export class AuthSigninUseCase implements SignIn {
   constructor(
     private readonly findUserByEmailRepository: FindUserByEmailRepository,
+    private readonly encrypter: CompareCrypto,
   ) {}
 
-  async signIn(data: AuthSignInModel.Params): Promise<AuthSignInModel.Result> {
+  async signIn(
+    data: AuthSignInModel.Params,
+  ): Promise<AuthSignInModel.Result | null> {
     const { email, password } = data;
     const user = await this.findUserByEmailRepository.findByEmail(email);
-    if (!user || user.password !== password) {
-      return null;
+    if (user) {
+      const comparePassword = await this.encrypter.compareHash(
+        password,
+        user.password,
+      );
     }
-
-    return new Promise((resolve) =>
-      resolve({
-        user: {
-          id: 'any_id',
-          name: 'any_name',
-          email: email,
-        },
-        access_token: 'any_token',
-      }),
-    );
+    return null;
   }
 }
