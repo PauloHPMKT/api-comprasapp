37b535c8f73cc43ade55888c747f42d2f90cf997
Paulo Sergio <pauloserg861@gmail.com>
2025-04-07 21:53:35 -0300

    test: ensure AuthSignIn throw if a user is not found

--- src/modules/auth/data/usecases/auth-signin.spec.ts
+++ src/modules/auth/data/usecases/auth-signin.spec.ts
@@ -1,14 +1,38 @@
+import { FindUserByEmailRepository } from '@/shared/services/user/protocols/find-user-by-email';
 import { AuthSigninUseCase } from './auth-signin';
+import { UserModel } from '@/modules/user/domain/models/user-model';
+
+const makeFindUserByEmail = (): FindUserByEmailRepository => {
+  class FindUserByEmailStub implements FindUserByEmailRepository {
+    async findByEmail(email: string): Promise<UserModel.Params> {
+      return new Promise((resolve) =>
+        resolve({
+          id: 'any_id',
+          name: 'any_name',
+          email: 'any_email@mail.com',
+          password: 'hashed_password',
+          avatar: null,
+          accountId: 'any_account_id',
+          createdAt: new Date('2025-01-01'),
+        }),
+      );
+    }
+  }
+  return new FindUserByEmailStub();
+};
 
 const makeSut = (): SutTypes => {
-  const sut = new AuthSigninUseCase();
+  const findUserByEmailRepositoryStub = makeFindUserByEmail();
+  const sut = new AuthSigninUseCase(findUserByEmailRepositoryStub);
   return {
     sut,
+    findUserByEmailRepositoryStub,
   };
 };
 
 type SutTypes = {
   sut: AuthSigninUseCase;
+  findUserByEmailRepositoryStub: FindUserByEmailRepository;
 };
 
 describe('AuthSigninUseCase', () => {
@@ -17,5 +41,22 @@ describe('AuthSigninUseCase', () => {
     expect(sut).toBeDefined();
     expect(sut).toBeInstanceOf(AuthSigninUseCase);
     expect(sut).toBeTruthy();
+  });
+
+  it('should throw if user is not found', async () => {
+    const { sut, findUserByEmailRepositoryStub } = makeSut();
+    jest
+      .spyOn(findUserByEmailRepositoryStub, 'findByEmail')
+      .mockImplementationOnce(() => {
+        throw new Error('User not found with this email');
+      });
+    const params = {
+      email: 'invalid_email@mail.com',
+      password: 'any_password',
+    };
+    const promise = sut.signIn(params);
+    await expect(promise).rejects.toThrow(
+      new Error('User not found with this email'),
+    );
   });
 });
