a4ef64fdc44a9f7b68fbc455ee9f713f83e44cb9
Paulo Sergio <pauloserg861@gmail.com>
2025-04-09 15:56:42 -0300

    refactor: adjust project structure

--- src/main/factories/modules/signup/data/add-signup-factory.ts
+++ src/main/factories/modules/signup/data/add-signup-factory.ts
@@ -1,32 +1,17 @@
-import { MongoAccountRepository } from '@/modules/account/infra/db/mongo/mongo-account-repository';
-import { AddSignupUseCase } from '@/modules/signup/data/usecases/add-signup';
 import { BcryptAdapter } from '@/modules/signup/infra/cryptography/bcrypt-adapter';
-import { AccountService } from '@/shared/services/account/account-service';
 import { makeUserServiceFactory } from '../../services/user/user-service-factory';
+import { makeAccountServiceFactory } from '../../services/account/account-service-factory';
+import { AddSignupUseCase } from '@/modules/signup/data/usecases/add-signup';
 
 const makeBcryptAdapterFactory = (): BcryptAdapter => {
   const salt = 12;
   return new BcryptAdapter(salt);
 };
 
-const makeUserServicesFactory = () => {
-  const userService = makeUserServiceFactory();
-
-  return {
-    verifyEmailService: userService,
-    addUserService: userService,
-  };
-};
-
-const makeAccountServiceFactory = () => {
-  const accountRepository = new MongoAccountRepository();
-  return new AccountService(accountRepository);
-};
-
 export const makeAddSignupUseCaseFactory = (): AddSignupUseCase => {
   const bcryptAdapter = makeBcryptAdapterFactory();
-  const verifyEmailRepository = makeUserServicesFactory().verifyEmailService;
-  const addUserRepository = makeUserServicesFactory().addUserService;
+  const verifyEmailRepository = makeUserServiceFactory();
+  const addUserRepository = makeUserServiceFactory();
   const addAccountService = makeAccountServiceFactory();
 
   return new AddSignupUseCase(
