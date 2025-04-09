24110d0178a66a51edb3f14799c87ed599d52113
Paulo Sergio <pauloserg861@gmail.com>
2025-04-09 08:59:54 -0300

    test: ensure MongoUserRepository return a valid user found by email

--- src/modules/user/infra/db/mongo/mongo-user-repository.spec.ts
+++ src/modules/user/infra/db/mongo/mongo-user-repository.spec.ts
@@ -79,4 +79,29 @@ describe('MongoUserRepository', () => {
     expect(account.avatar).toBeNull();
     expect(account.accountId).toBeTruthy();
   });
+
+  it('should return a valid user found by email', async () => {
+    const sut = makeSut();
+    jest.spyOn(sut, 'findByEmail').mockResolvedValueOnce({
+      id: new ObjectId().toHexString(),
+      name: 'any_name',
+      email: 'any_email@mail.com',
+      password: 'any_password',
+      avatar: null,
+      accountId: new ObjectId().toHexString(),
+      createdAt: expect.any(Date),
+    });
+    const getCollection = MongoHelper.getCollection('users');
+    await getCollection.insertOne({
+      id: 'any_id',
+      name: 'any_name',
+      email: 'any_email@mail.com',
+      password: 'any_password',
+      avatar: null,
+      accountId: 'any_account_id',
+      createdAt: new Date('2025-12-10'),
+    });
+    const user = await sut.findByEmail('any_email@mail.com');
+    expect(user).toBeTruthy();
+  });
 });
