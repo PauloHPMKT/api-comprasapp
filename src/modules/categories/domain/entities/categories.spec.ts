6d0c4fd6d59a3a04712e6bc12a29960f9de4c41e
Paulo Sergio <pauloserg861@gmail.com>
2025-05-15 21:54:06 -0300

    test: ensure a new Category is created with name

--- src/modules/categories/domain/entities/categories.spec.ts
+++ src/modules/categories/domain/entities/categories.spec.ts
@@ -1,7 +1,9 @@
 import { Category } from './Category';
 
 const makeSut = (): Category => {
-  return new Category();
+  return new Category({
+    name: 'Category name',
+  });
 };
 
 describe('Categories Entity', () => {
@@ -10,5 +12,10 @@ describe('Categories Entity', () => {
     expect(sut).toBeDefined();
     expect(sut).toBeInstanceOf(Category);
     expect(sut).toBeTruthy();
+  });
+
+  it('should create a new Category by name', () => {
+    const sut = makeSut();
+    expect(sut.props.name).toEqual('Category name');
   });
 });
