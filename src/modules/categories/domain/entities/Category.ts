854bb3257795986d7234633c7ecc034a1155affd
Paulo Sergio <pauloserg861@gmail.com>
2025-05-20 08:44:39 -0300

    feat: ensure a new Category is created with a valid date

--- src/modules/categories/domain/entities/Category.ts
+++ src/modules/categories/domain/entities/Category.ts
@@ -1,8 +1,11 @@
 export type CategoryProps = {
   name: string;
   icon: string;
+  createdAt?: Date;
 };
 
 export class Category {
-  constructor(public readonly props: CategoryProps) {}
+  constructor(public readonly props: CategoryProps) {
+    this.props.createdAt = this.props.createdAt ?? new Date();
+  }
 }
