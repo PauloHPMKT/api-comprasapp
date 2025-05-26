6a9744e9ce85f3b64b882735f087c1dcbc9fd0c3
Paulo Sergio <pauloserg861@gmail.com>
2025-05-26 08:19:06 -0300

    feat: ensure CreateCategoryController return 500 if AcreateCategoryRepository throws

--- src/modules/categories/presentation/controller/create-category.ts
+++ src/modules/categories/presentation/controller/create-category.ts
@@ -1,5 +1,8 @@
 import { MissingParamError } from '@/shared/presentation/errors';
-import { badRequest } from '@/shared/presentation/helper/http-responses';
+import {
+  badRequest,
+  serverError,
+} from '@/shared/presentation/helper/http-responses';
 import { Controller } from '@/shared/presentation/protocols/controller';
 import { HttpRequest, HttpResponse } from '@/shared/presentation/types/http';
 import { CategoryModel } from '../../domain/models/category';
@@ -13,16 +16,21 @@ export class CreateCategoryController extends Controller<CategoryModel.Params> {
   async handle(
     request: HttpRequest<CategoryModel.Params>,
   ): Promise<HttpResponse> {
-    const requiredFields = ['name', 'icon'];
-    const error = this.validateRequiredFields(request, requiredFields);
-    if (error) {
-      return badRequest(new MissingParamError(error));
-    }
+    try {
+      const requiredFields = ['name', 'icon'];
+      const error = this.validateRequiredFields(request, requiredFields);
+      if (error) {
+        return badRequest(new MissingParamError(error));
+      }
 
-    const { name, icon } = request.body;
-    await this.createCategory.execute({
-      name,
-      icon,
-    });
+      const { name, icon } = request.body;
+      await this.createCategory.execute({
+        name,
+        icon,
+      });
+    } catch (error) {
+      console.error(error);
+      return serverError();
+    }
   }
 }
