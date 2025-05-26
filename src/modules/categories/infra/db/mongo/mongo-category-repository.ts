e232365fc0c65ab3b386d9a6c6ddbf85252b330f
Paulo Sergio <pauloserg861@gmail.com>
2025-05-26 08:45:06 -0300

    feat: ensure a nrew category is created on success

--- src/modules/categories/infra/db/mongo/mongo-category-repository.ts
+++ src/modules/categories/infra/db/mongo/mongo-category-repository.ts
@@ -1,1 +1,21 @@
-export class MongoCategoryRepository {}
+import { CategoryRepoModel } from '@/modules/categories/data/models/category';
+import { CreateCategoryRepository } from '@/modules/categories/data/protocols/create-category-repository';
+import { MongoHelper } from '@/shared/infra/db/helper/mongo-client';
+
+export class MongoCategoryRepository implements CreateCategoryRepository {
+  async create(
+    params: CategoryRepoModel.Params,
+  ): Promise<CategoryRepoModel.Result> {
+    const categoryCollection = MongoHelper.getCollection('categories');
+    const { insertedId } = await categoryCollection.insertOne({
+      _id: MongoHelper.toObjectId(params.id),
+      name: params.name,
+      icon: params.icon,
+      createdAt: params.createdAt,
+    });
+    const category = await categoryCollection.findOne({
+      _id: insertedId,
+    });
+    return MongoHelper.map(category) as CategoryRepoModel.Result;
+  }
+}
