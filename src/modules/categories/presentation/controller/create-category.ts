67ba271ba16a98d186291fe0335e527841b93237
Paulo Sergio <pauloserg861@gmail.com>
2025-05-25 21:57:49 -0300

    feat: ensure CreateCategoryController return 400 if no name is provided

--- src/modules/categories/presentation/controller/create-category.ts
+++ src/modules/categories/presentation/controller/create-category.ts
@@ -1,1 +1,17 @@
-export class CreateCategoryController {}
+import { MissingParamError } from '@/shared/presentation/errors';
+import { badRequest } from '@/shared/presentation/helper/http-responses';
+import { Controller } from '@/shared/presentation/protocols/controller';
+import { HttpRequest, HttpResponse } from '@/shared/presentation/types/http';
+
+interface CreateCategoryDTO {
+  name: string;
+  icon: string;
+}
+
+export class CreateCategoryController extends Controller {
+  async handle(request: HttpRequest<CreateCategoryDTO>): Promise<HttpResponse> {
+    if (!request.body.name) {
+      return badRequest(new MissingParamError('name'));
+    }
+  }
+}
