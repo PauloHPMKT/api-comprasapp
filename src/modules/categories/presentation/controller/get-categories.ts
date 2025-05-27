5096b433df3a9257fda7b15dc320f75ccafa7e8b
Paulo Sergio <pauloserg861@gmail.com>
2025-05-26 22:19:16 -0300

    feat: ensure GetCategoriesController return an empty array if no categories is found

--- src/modules/categories/presentation/controller/get-categories.ts
+++ src/modules/categories/presentation/controller/get-categories.ts
@@ -8,29 +8,10 @@ export class GetCategoriesController extends Controller {
   }
 
   async handle(): Promise<HttpResponse> {
-    await this.getCategories.execute();
+    const categories = await this.getCategories.execute();
     return {
       statusCode: 200,
-      body: [
-        {
-          id: '1',
-          name: 'Category 1',
-          icon: '🚿',
-          createdAt: new Date('2023-01-01T00:00:00Z'),
-        },
-        {
-          id: '2',
-          name: 'Category 2',
-          icon: '🥦',
-          createdAt: new Date('2023-02-01T00:00:00Z'),
-        },
-        {
-          id: '3',
-          name: 'Category 3',
-          icon: '🍎',
-          createdAt: new Date('2023-03-01T00:00:00Z'),
-        },
-      ],
+      body: categories,
     };
   }
 }
