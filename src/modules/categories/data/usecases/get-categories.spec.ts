5c32568aae6a804af8e22e6bf74447f1b0c024ef
Paulo Sergio <pauloserg861@gmail.com>
2025-05-27 08:08:20 -0300

    feat: ensure GetCategoriesUseCase calls GetCategoriesRepository on success

--- src/modules/categories/data/usecases/get-categories.spec.ts
+++ src/modules/categories/data/usecases/get-categories.spec.ts
@@ -1,14 +1,33 @@
+import { GetCategoriesRepository } from '../protocols/get-categories-repository';
 import { GetCategoriesUseCase } from './get-categories';
 
+const makeGetCategoriesRepository = (): GetCategoriesRepository => {
+  class GetCategoriesRepositoryStub implements GetCategoriesRepository {
+    async findAll(): Promise<any[]> {
+      const createdAt = new Date('2023-01-01T00:00:00Z');
+      return new Promise((resolve) =>
+        resolve([
+          { id: '1', name: 'Category 1', icon: '🚿', createdAt },
+          { id: '2', name: 'Category 2', icon: '🥦', createdAt },
+        ]),
+      );
+    }
+  }
+  return new GetCategoriesRepositoryStub();
+};
+
 const makeSut = (): SutTypes => {
-  const sut = new GetCategoriesUseCase();
+  const getCategoriesRepositoryStub = makeGetCategoriesRepository();
+  const sut = new GetCategoriesUseCase(getCategoriesRepositoryStub);
   return {
     sut,
+    getCategoriesRepositoryStub,
   };
 };
 
 interface SutTypes {
   sut: GetCategoriesUseCase;
+  getCategoriesRepositoryStub: GetCategoriesRepository;
 }
 
 describe('GetCategoriesUseCase', () => {
@@ -17,5 +36,12 @@ describe('GetCategoriesUseCase', () => {
     expect(sut).toBeDefined();
     expect(sut).toBeInstanceOf(GetCategoriesUseCase);
     expect(sut).toBeTruthy();
+  });
+
+  it('should call getCategoriesRepository findAll with success', async () => {
+    const { sut, getCategoriesRepositoryStub } = makeSut();
+    jest.spyOn(getCategoriesRepositoryStub, 'findAll');
+    await sut.execute();
+    expect(getCategoriesRepositoryStub.findAll).toHaveBeenCalledTimes(1);
   });
 });
