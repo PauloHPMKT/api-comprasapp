9323c45411e5bc82d7ccedb30398e37faa2bf210
Paulo Sergio <pauloserg861@gmail.com>
2025-05-27 08:07:34 -0300

    feat: ensure GetCategoriesUseCase calls GetCategoriesRepository on success

--- src/modules/categories/data/usecases/get-categories.ts
+++ src/modules/categories/data/usecases/get-categories.ts
@@ -1,1 +1,12 @@
-export class GetCategoriesUseCase {}
+import { GetCategories } from '../../domain/usecases/get-categories';
+import { GetCategoriesRepository } from '../protocols/get-categories-repository';
+
+export class GetCategoriesUseCase implements GetCategories {
+  constructor(
+    private readonly getCategoriesRepository: GetCategoriesRepository,
+  ) {}
+
+  async execute(): Promise<any> {
+    await this.getCategoriesRepository.findAll();
+  }
+}
