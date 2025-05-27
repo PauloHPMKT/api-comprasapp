2fc18fbb75a9f5205e11f9ecb0bb93549f41a9c4
Paulo Sergio <pauloserg861@gmail.com>
2025-05-26 22:28:31 -0300

    feat: ensure GetCategoriesController return 500 if an error throw

--- src/modules/categories/presentation/controller/get-categories.ts
+++ src/modules/categories/presentation/controller/get-categories.ts
@@ -1,6 +1,7 @@
 import { Controller } from '@/shared/presentation/protocols/controller';
 import { HttpResponse } from '@/shared/presentation/types/http';
 import { GetCategories } from '../../domain/usecases/get-categories';
+import { serverError } from '@/shared/presentation/helper/http-responses';
 
 export class GetCategoriesController extends Controller {
   constructor(private readonly getCategories: GetCategories) {
@@ -8,10 +9,15 @@ export class GetCategoriesController extends Controller {
   }
 
   async handle(): Promise<HttpResponse> {
-    const categories = await this.getCategories.execute();
-    return {
-      statusCode: 200,
-      body: categories,
-    };
+    try {
+      const categories = await this.getCategories.execute();
+      return {
+        statusCode: 200,
+        body: categories,
+      };
+    } catch (error) {
+      console.error(error);
+      return serverError();
+    }
   }
 }
