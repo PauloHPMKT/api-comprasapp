547776f06984b0048bf9fd0d9ff0dad49f85554c
Paulo Sergio <pauloserg861@gmail.com>
2025-05-26 21:47:29 -0300

    feat: ensure GetCategoriesController return 200 and a list of categories on success

--- src/modules/categories/presentation/controller/get-categories.ts
+++ src/modules/categories/presentation/controller/get-categories.ts
@@ -1,1 +1,30 @@
-export class GetCategoriesController {}
+import { Controller } from '@/shared/presentation/protocols/controller';
+import { HttpRequest, HttpResponse } from '@/shared/presentation/types/http';
+
+export class GetCategoriesController extends Controller {
+  async handle(): Promise<HttpResponse> {
+    return {
+      statusCode: 200,
+      body: [
+        {
+          id: '1',
+          name: 'Category 1',
+          icon: '🚿',
+          createdAt: new Date('2023-01-01T00:00:00Z'),
+        },
+        {
+          id: '2',
+          name: 'Category 2',
+          icon: '🥦',
+          createdAt: new Date('2023-02-01T00:00:00Z'),
+        },
+        {
+          id: '3',
+          name: 'Category 3',
+          icon: '🍎',
+          createdAt: new Date('2023-03-01T00:00:00Z'),
+        },
+      ],
+    };
+  }
+}
