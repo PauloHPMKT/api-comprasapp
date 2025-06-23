491ba12a3ea5b93f36f3d84fc2ccecd05cf9c42a
Paulo Sergio <pauloserg861@gmail.com>
2025-06-23 10:51:49 -0300

    feat: ensure SignupController return 404 if no name is provided

--- src/modules/signup/presentation/signup.controller.ts
+++ src/modules/signup/presentation/signup.controller.ts
@@ -1,4 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 
 @Controller('signup')
-export class SignupController {}
+export class SignupController {
+  @Post()
+  async handle(@Body() params: any): Promise<any> {
+    if (!params.body.name) {
+      return {
+        statusCode: 404,
+        body: new Error('Name is required'),
+      };
+    }
+  }
+}
